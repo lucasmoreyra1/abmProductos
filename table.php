b45257dd4d3628ad0dae7c7dab539e639f130dfe
Lucas Moreyra <lucasmoreyra1703@gmail.com>
2023-04-12 00:05:54 -0300

    buscador agregado

--- table.php
+++ table.php
@@ -1,5 +1,11 @@
-<div class="container-fluid p-3">
-    <table class="table table-sm text-center table-bordered">
+<form>
+
+    Texto a buscar <input id="searchTerm" type="text" onkeyup="doSearch()" />
+
+</form>
+
+<div class="container-fluid p-3" >
+    <table class="table table-sm text-center table-bordered" id="datos">
         <tr class="table-active">
             <td>Id producto</td>
             <td>Nombre</td>
@@ -26,16 +32,24 @@
             <td> <button class="btn btn-outline-primary" onclick="create(`<?php echo $key['nombre']?>`, <?php echo $key['precio']?>, `<?php echo $key['descripcion']?>`, <?php echo $key['stock']?>, `<?php echo $key['marca_nombre']?>`, <?php echo $key['id_producto']?>, 'actualizar')"><i class="bi bi-pencil"></i></button> </td>
 
         </tr>
+
+
     <?php endforeach;
         require 'script.php';
 
     ?>
+        <tr class='noSearch hide'>
+
+            <td colspan="8"></td>
+
+        </tr>
     </table>
 </div>
 
 <form action="./logic.php" method="post">
     <section class="container"></section>
 </form>
+
 
 
 
